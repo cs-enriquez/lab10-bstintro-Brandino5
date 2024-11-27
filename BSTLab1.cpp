a7a2938ad04b9900d997b51af97e9dcbda7d1e18
Brandon Davila <156960005+Brandino5@users.noreply.github.com>
2024-11-27 20:24:26 +0000

    Complete

--- BSTLab1.cpp
+++ BSTLab1.cpp
@@ -5,6 +5,10 @@ using namespace std;
 // Implement the node class we will need for our Binary Search Tree
 class Node {
 	public:
+		int key;
+		Node* left;
+		Node* right;
+
 		// the Node should contain
 		// 	- an integer "key" that holds the value of the Node
 		// 	- a pointer to each child possible child node ("right" and "left")
@@ -13,6 +17,11 @@ class Node {
 		// Write a constructor for the Node
 		// input: only an integer value for "key", no pointer arguments
 		// 		right and left pointers should be null
+		Node(int key){ 
+			this ->key = key;
+			this-> left = nullptr;
+			this->right = nullptr;
+		}
 
 };
 
@@ -21,21 +30,32 @@ class Node {
 // {13, 0, 7, 6, 21, 15, -2, 12, 99, 18, 19, -1}
 Node* createTree() {
 	// root
-	Node *root = nullptr;
+	Node *root = new Node(13);
 
 	// level 1 (children of root)
+	root->left = new Node(0);
+	root->right = new Node(21);
 
 	// level 2 (children of 0)
+	root->left->left = new Node(-2);
+	root->right->right = new Node(7);
 
 	// level 2 (children of 21)
+	root->right->left = new Node(15);
+	root->right->right = new Node(99);
 
 	// level 3 (children of -2)
+	root->right->left = new Node(-1);
 
 	// level 3 (children of 7)
+	root->left->right->left = new Node(6);
 
 	// level 3 (children of 15)
+	root->right->left->left = new Node(12);
 
 	// level 3 (children of 99)
+	root->right->right->left = new Node(18);
+	root->right->right->left->right = new Node(19);
 
 	// level 4 (children of 18)
 
@@ -62,18 +82,26 @@ bool searchTree(int target, Node* root) {
 // You should write this function recursively!
 int treeSize(Node* root) {
 	// base case
-	return -1;
+	if( root == nullptr){
+		return 0;
+	}
+
 
 	// General case
-	return -1;
+	return 1 + treeSize(root-> left) + treeSize(root-> right);
 
 }
 
 // BONUS! Write a function that will determine the height of the tree
 int treeHeight(Node* root) {
 	// base case
-	return -1;
+	if ( root == nullptr){
+		return -1;
+	}
 
 	// General case
-	return -1;
+	int leftHeight = treeHeight(root->left);
+	int rightHeight = treeHeight(root->right);
+
+	return 1 + std::max(leftHeight, rightHeight);
 }
